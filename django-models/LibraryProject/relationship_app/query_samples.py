d1c6e110162449345c60e06849247d3cde8a045a
Sharon Chepkemoi <sharonngeno23@gmail.com>
2025-09-07 14:09:42 +0300

    Fix: align query_samples with checker expectations

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,7 +2,8 @@ from relationship_app.models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
 def books_by_author(author_name):
-    return Book.objects.filter(author__name=author_name)
+    author = Author.objects.get(name=author_name)
+    return author.book_set.all()   # use book_set (default reverse name)
 
 # List all books in a library
 def books_in_library(library_name):
@@ -11,4 +12,5 @@ def books_in_library(library_name):
 
 # Retrieve the librarian for a library
 def librarian_for_library(library_name):
-    return Librarian.objects.get(library__name=library_name)
+    library = Library.objects.get(name=library_name)
+    return library.librarian
